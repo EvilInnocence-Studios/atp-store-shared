b75a7989f480a09faa25b744fadbb868dcadfe82
Andrew Wittrock <andy@wittrock.us>
2024-12-19 23:52:58 -0600

    Initial commit

---
+++ product/types.d.ts
@@ -0,0 +1,24 @@
+import { NewObj } from "../../core-shared/express/types";
+
+export declare interface IProduct {
+    id: number;
+    name: string;
+    sku: string;
+    url: string;
+    description: string;
+    descriptionShort: string;
+    productType: "digital" | "grouped";
+    subscriptionOnly: boolean;
+    releaseDate: string;
+    brokeredAt: string | null;
+    brokerageProductId: string | null;
+    price: number;
+    enabled: boolean;
+    metaTitle: string | null;
+    metaDescription: string | null;
+    metaKeywords: string | null;
+    thumbnail: string | null;
+    thumbnailCaption: string | null;
+}
+
+export type NewProduct = NewObj<IProduct>;
