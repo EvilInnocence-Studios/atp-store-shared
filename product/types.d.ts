f42de9ac9633fb8fae2ad70ba5be2f12248a449e
Andrew Wittrock <andy@wittrock.us>
2024-12-26 14:19:21 -0600

    Add thumbnailUrl to full product type

--- product/types.d.ts
+++ product/types.d.ts
@@ -23,6 +23,7 @@ export declare interface IProduct {
 
 export declare interface IProductFull extends IProduct {
     tags: string[];
+    thumbnailUrl: string;
 }
 
 export type NewProduct = Partial<NewObj<IProduct>>;
