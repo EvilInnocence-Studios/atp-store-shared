eeac24228976f1cb58a6b6d71d00e21bcad75db2
Andrew Wittrock <andy@wittrock.us>
2025-01-31 13:37:18 -0600

    Refactor order types

--- product/types.d.ts
+++ product/types.d.ts
@@ -43,39 +43,3 @@ export declare interface IProductFile {
     folder: string;
 }
 
-export declare interface IOrder {
-    id: number;
-    userId: number;
-    total: number;
-    subtotal: number;
-    discount: number;
-    status: "pending" | "complete";
-    transactionId: string | null;
-    couponCode: string;
-    createdAt: string;
-}
-
-export type NewOrder = Partial<NewObj<IOrder>>;
-
-export declare interface IOrderItem {
-    id: number;
-    orderId: number;
-    productId: number;
-    quantity: number;
-}
-
-export declare interface IOrderFull extends IOrder {
-    items: IProduct[];
-    files: IProductFile[];
-}
-
-export declare interface IWishlistItem {
-    id: number;
-    userId: number;
-    productId: number;
-}
-
-export declare interface IOrderCreateRequest {
-    ids: number[];
-    couponCode: string;
-}
