9635968d6c665db29baf978de44b8dd2bdeb8eb3
Andrew Wittrock <andy@wittrock.us>
2025-01-31 22:20:28 -0600

    Auto stash before merge of "main" and "origin/main"

--- discount/flatPercentage.ts
+++ discount/flatPercentage.ts
@@ -1,21 +1,25 @@
 import { prop } from "ts-functional";
 import { IDiscount, IDiscountCalculator } from "./types";
 import { IPermission } from "../../uac-shared/permissions/types";
+import { debugPort } from "process";
 
 export const FlatPercentageProductDiscount = (percentage: number, permissionId:number | null, userPermissions: IPermission[]):IDiscountCalculator => ({
+    name: "FlatPercentageProductDiscount",
     productSalePrice: (product, price) =>
         (!permissionId || userPermissions.map(prop("id")).includes(permissionId)) && !product.brokeredAt
             ? price * (1 - percentage)
             : price,
-    cartDiscount: (_products, _subtotal) => 0.0,
+    cartDiscount: (_products, _subtotal, _discount) => 0.0,
 });
 
 export const FlatPercentageCartDiscount = (percentage: number, permissionId: number | null, userPermissions: IPermission[]):IDiscountCalculator => ({
-    productSalePrice: (_product, price) => price,
-    cartDiscount: (_products, subtotal) => 
-        !permissionId || userPermissions.map(prop("id")).includes(permissionId)
-            ? subtotal * percentage
-            : 0.0,
+    name: "FlatPercentageCartDiscount",
+    productSalePrice: (_product, _price) => 0.0,
+    cartDiscount: (_products, subtotal, discount) => {
+        return !permissionId || userPermissions.map(prop("id")).includes(permissionId)
+            ? discount + (subtotal - discount) * percentage
+            : discount;
+    },
 });
 
 export const getCalculator = (discount:IDiscount, userPermissions:IPermission[]):IDiscountCalculator => 
--- discount/types.d.ts
+++ discount/types.d.ts
@@ -1,13 +1,15 @@
 import { IPermission } from "../../uac-shared/permissions/types";
 import { IProduct } from "../product/types";
 
-// Note: we pass the current price to these functions, so that discounts can be combined,
 export declare interface IDiscountCalculator {
+    name: string;
     // For discounts that only depend on a single product
+    // The function should return the new price based on the product and current discounted price
     productSalePrice: (product:IProduct, price: number) => number;
 
     // For discounts that depend on the entire cart, such as BOGO or percentage off the entire cart
-    cartDiscount: (products:IProduct[], subtotal: number) => number;
+    // The function should return the new discount based on the product, undiscounted subtotal, and current discount
+    cartDiscount: (products:IProduct[], subtotal: number, discount: number) => number;
 }
 
 export declare interface IDiscount {
